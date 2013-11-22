1dfd6cd7fdc8e83cd98d6b9bf8d8105df9e1ee54
seanlewis <sean.lewis@youngshand.com>
2013-11-22 13:55:49 +1300

    initial commit

---
+++ ads.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+
+from flask import Flask, render_template, request
+
+import MySQLdb as mysql
+
+app = Flask(__name__)
+
+@app.route("/test")
+def hello():
+    return "Hello World!"
+
+@app.route("/")
+def index():
+	connection = mysql.connect("127.0.0.1", "root", "root", "media.yng.sh", 3306)
+	c = connection.cursor(mysql.cursors.DictCursor)
+	
+	q = "SELECT * FROM media"
+
+	if request.args.get('category', False):
+		q += " WHERE category = '%s'" % request.args.get("category")
+
+	c.execute(q)
+	media = c.fetchall()
+
+	cat = "SELECT distinct(category) from media"
+	c.execute(cat)
+	category = c.fetchall()
+
+	connection.close()
+	return render_template("index.html", media=media, categories=category)
+
+if __name__ == "__main__":
+	app.run(debug = True)
